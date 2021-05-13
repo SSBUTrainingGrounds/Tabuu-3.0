d8a8cb676f4e120ce4a4543769b50021d0fb9497
sonnenbankpimp <80397293+sonnenbankpimp@users.noreply.github.com>
2021-05-13 18:08:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Tabuu 3.0
 #by Phxenix for SSBU Training Grounds
-#Version: 3.9.4
-#Last Changes: 5 May 2021
+#Version: 3.10.0
+#Last Changes: 13 May 2021
 #Report any bugs to: Phxenix#1104
 #
 #To do list:
@@ -24,7 +24,7 @@ bot = Bot(command_prefix='%', intents=intents) # prefix for commands, we picked 
 client = discord.Client(intents=intents) 
 bot.remove_command('help') #for a custom help command
 
-bot.version_number = "3.9.4" #the "version", maintain every now and then
+bot.version_number = "3.10.0" #the "version", maintain every now and then
 
 
 #bot startup, and some event triggers without commands
@@ -72,6 +72,9 @@ async def admin(ctx):
 ```%records``` - Shows ban records.\n\
 ```%forcereportmatch <@winner> <@loser>``` - If someone abandons a ranked match.\n\
 ```%leaderboard``` - Leaderboards of ranked matchmaking\n\
+```%newrolemenu <message ID> <emoji> <role>``` - Adds an entry for a role menu.\n\
+```%deleterolemenu <message ID>``` - Deletes every entry for a Message with a role menu.\n\
+```%geteveryrolemenu``` - Gets you every role menu entry currently active.\n\
         ')
     await ctx.author.send(embed=embed)
     await ctx.message.add_reaction('👍')
