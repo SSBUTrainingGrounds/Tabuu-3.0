bd4d3206daf15744530831bb57c76c0f73fd8874
sonnenbankpimp <80397293+sonnenbankpimp@users.noreply.github.com>
2021-06-16 23:32:30 +0200

    Add files via upload

--- cogs/logging.py
+++ cogs/logging.py
@@ -139,7 +139,10 @@ class Logging(commands.Cog):
         embed.set_author(name=f"{str(message.author)} ({message.author.id})", icon_url=message.author.avatar_url)
         if message.attachments: #if a message has an attachment with it, it also gets logged. i dont really care for that on the on message edit though
             if len(message.attachments) == 1: #for one attachment, this is enough
-                embed.add_field(name="Attachment", value=message.attachments[0].url)
+                if message.attachments[0].url.endswith(('.jpg', '.png', '.jpeg')): #if its an image it will be the embed image
+                    embed.set_image(url=message.attachments[0].url)
+                else: #else it will just log the url
+                    embed.add_field(name="Attachment", value=message.attachments[0].url)
             else: #for multiple, putting it all into one embed value might result in too many characters, so this is needed. only mobile users can send multiple attachments anyway right now
                 i = 1
                 for x in message.attachments:
