f569c49cf6c1d07d6635a23c6df2b94ee2e68e48
sonnenbankpimp <80397293+sonnenbankpimp@users.noreply.github.com>
2021-08-09 01:59:28 +0200

    split up stats commands into new file

--- cogs/usercommands.py
+++ cogs/usercommands.py
@@ -1,14 +1,7 @@
 import discord
 from discord.ext import commands, tasks
 import random
-from fuzzywuzzy import process
-from discord.utils import get
-import platform
 import asyncio
-import psutil
-import datetime
-import os
-import time
 
 
 #
@@ -18,62 +11,6 @@ import time
 class Usercommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command() #some basic info about a role
-    async def roleinfo(self, ctx, *,input_role):
-        unwanted = ['<','@','>', '&']
-        for i in unwanted:
-            input_role = input_role.replace(i,'')
-        all_roles = []
-
-        for role in ctx.guild.roles:
-            all_roles.append(role.name)
-        try:
-            role = get(ctx.guild.roles, id=int(input_role))
-        except:
-            match = process.extractOne(input_role, all_roles, score_cutoff=30)[0]
-            role = get(ctx.guild.roles, name=match)
-
-        #the above block searches all roles for the closest match, as seen in the admin cog
-
-        embed = discord.Embed(title=f"Roleinfo of {role.name} ({role.id})", color = role.colour)
-        embed.add_field(name="Role Name:", value=role.mention, inline=True)
-        embed.add_field(name="Users with role:", value=len(role.members), inline=True)
-        embed.add_field(name="Created at:", value=discord.utils.format_dt(role.created_at, style='F'))
-        embed.add_field(name="Mentionable:", value=role.mentionable, inline=True)
-        embed.add_field(name="Displayed Seperately:", value=role.hoist, inline=True)
-        embed.add_field(name="Color:", value=role.color, inline=True)
-        await ctx.send(embed=embed)
-
-    @commands.command(aliases=['listroles']) #lists every member in the role if there arent more than 60 members, to prevent spam
-    async def listrole(self, ctx, *,input_role):
-        unwanted = ['<','@','>', '&']
-        for i in unwanted:
-            input_role = input_role.replace(i,'')
-        all_roles = []
-
-        for role in ctx.guild.roles:
-            all_roles.append(role.name)
-        try:
-            role = get(ctx.guild.roles, id=int(input_role))
-        except:
-            match = process.extractOne(input_role, all_roles, score_cutoff=30)[0]
-            role = get(ctx.guild.roles, name=match)
-
-        members = role.members
-        memberlist = []
-
-        if len(members) > 60:
-            await ctx.send(f"Users with the {role} role ({len(role.members)}):\nToo many users to list!")
-            return
-        if len(members) == 0:
-            await ctx.send(f"No user currently has the {role} role!")
-            return
-        else:
-            for member in members:
-                memberlist.append(f"{member.name}#{member.discriminator}")
-            all_members = ', '.join(memberlist)
-            await ctx.send(f"Users with the {role} role ({len(role.members)}):\n{all_members}")
 
 
     #modmail
@@ -95,84 +32,14 @@ class Usercommands(commands.Cog):
             await ctx.send("For the sake of privacy, please only use this command in my DM's. They are always open for you.")
     
 
-    #serverinfo, gives out basic info about the server
-    @commands.command(aliases=['serverinfo'])
-    async def server(self, ctx):
-        server = ctx.guild
-        embed = discord.Embed(title=f"{server.name}({server.id})", color=discord.Color.green())
-        embed.add_field(name="Created on:", value=discord.utils.format_dt(server.created_at, style='F'), inline=True) #timezone aware datetime object, F is long formatting
-        embed.add_field(name="Owner:", value=server.owner.mention, inline=True)
-        embed.add_field(name="Channels:", value=len(server.channels), inline=True)
-        embed.add_field(name="Members:", value=f"{len(server.members)} (Bots: {sum(member.bot for member in server.members)})")
-        embed.add_field(name="Emojis:", value=len(server.emojis))
-        embed.add_field(name="Roles:", value=len(server.roles))
-        embed.set_thumbnail(url=server.icon.url)
-        await ctx.send(embed=embed)
-
-    #userinfo
-    @commands.command(aliases=['user'])
-    async def userinfo(self, ctx, member:discord.Member = None):
-        if member is None:
-            member = ctx.author
-
-        try:
-            activity = member.activity.name
-        except:
-            activity = "None"
-
-        if not ctx.guild:
-            await ctx.send("This command can only be used in the SSBU TG Discord Server.")
-            return
-
-        sorted_members = sorted(ctx.guild.members, key=lambda x:x.joined_at)
-        index = sorted_members.index(member)
-
-
-        embed = discord.Embed(title=f"Userinfo of {member.name}#{member.discriminator} ({member.id})", color=member.top_role.color)
-        embed.add_field(name="Name:", value=member.mention, inline=True)
-        embed.add_field(name="Top Role:", value=member.top_role.mention, inline=True)
-        embed.add_field(name="Number of Roles:", value=f"{(len(member.roles)-1)}", inline=True) #gives the number of roles to prevent listing like 35 roles, -1 for the @everyone role
-        embed.add_field(name="Joined Server on:", value=discord.utils.format_dt(member.joined_at, style='F'), inline=True) #timezone aware datetime object, F is long formatting
-        embed.add_field(name="Join Rank:", value=f"{(index+1)}/{len(ctx.guild.members)}", inline=True)
-        embed.add_field(name="Joined Discord on:",  value=discord.utils.format_dt(member.created_at, style='F'), inline=True)
-        embed.add_field(name="Online Status:", value=member.status, inline=True)
-        embed.add_field(name="Activity Status:", value=activity, inline=True)
-        embed.set_thumbnail(url=member.avatar.url)
-        await ctx.send(embed=embed)
-
-    #some bot stats
-    @commands.command(aliases=['stats'])
-    async def botstats(self, ctx):
-        pyversion = platform.python_version() #python version
-        dpyversion = discord.__version__ #discord.py version
-        servercount = len(self.bot.guilds) #total servers
-        membercount = len(set(self.bot.get_all_members())) #total members
-        proc = psutil.Process(os.getpid()) #gets process id
-        uptimeSeconds = time.time() - proc.create_time() #gets uptime in seconds
-        delta = datetime.timedelta(seconds=uptimeSeconds) #converts that to a timedelta object
-        tabuu3 = self.bot.get_user(785303736582012969) #the bot
-        embed = discord.Embed(title="Tabuu 3.0 Stats", color=0x007377, url="https://github.com/sonnenbankpimp/Tabuu-3.0-Bot") #link to the github, its still private but maybe not in the future, who knows
-        embed.add_field(name="Name:", value=f"{tabuu3.mention}", inline=True)
-        embed.add_field(name="Servers:", value=servercount, inline=True)
-        embed.add_field(name="Total Users:", value=membercount, inline=True)
-        embed.add_field(name="Bot Version:", value=self.bot.version_number, inline=True)
-        embed.add_field(name="Python Version:", value=pyversion, inline=True)
-        embed.add_field(name="discord.py Version:", value=dpyversion, inline=True)
-        embed.add_field(name="CPU Usage:", value=f"{psutil.cpu_percent(interval=1)}%", inline=True) #only gets the % value
-        embed.add_field(name="RAM Usage:", value=f"{psutil.virtual_memory()[2]}%", inline=True) #only gets the % value, thats what the [2] is for
-        embed.add_field(name="Uptime:", value=str(delta).split(".")[0], inline=True) #the split thing is to get rid of the microseconds, who cares about uptime in microseconds
-        embed.set_footer(text="Creator: Phxenix#1104, hosted on: Raspberry Pi 3B+")
-        embed.set_thumbnail(url=tabuu3.avatar.url)
-        await ctx.send(embed=embed)
-
-
+    #just returns the avatar of a user
     @commands.command(aliases=['icon'])
     async def avatar(self, ctx, member:discord.Member = None):
         if member is None:
             member = ctx.author
         await ctx.send(member.avatar.url)
 
-
+    #makes a basic poll
     @commands.command()
     async def poll(self, ctx, question, *options: str):
         if len(options) < 2:
@@ -253,7 +120,7 @@ class Usercommands(commands.Cog):
         else:
             await ctx.send(f"Rolling **1**-**{sides}** **{r+1}** times \nResults: **{results}** \nTotal: **{sum(results)}**")
 
-
+    #generic countdown
     @commands.command()
     async def countdown(self, ctx, count:int):
         if count > 50:
@@ -275,7 +142,7 @@ class Usercommands(commands.Cog):
         await asyncio.sleep(2) #sleeps again before sending the final message
         await countdown_message.edit(content=f"Counting down from {initial_count}...\nFinished!")
 
-
+    #reminder, just waits out the time, no fancy stuff
     @commands.command(aliases=['remindme'])
     async def reminder(self, ctx, time, *, remind_message):
         #this here gets the time
@@ -308,7 +175,7 @@ class Usercommands(commands.Cog):
 
         await ctx.send(f"{ctx.author.mention}, you wanted me to remind you of `{remind_message}`, {reminder_time} ago.")
 
-
+    #info about an emoji
     @commands.command(aliases=['emoji'])
     async def emote(self, ctx, emoji:discord.PartialEmoji):
         embed = discord.Embed(title="Emoji Info", colour=discord.Colour.orange(), description=f"\
@@ -320,7 +187,7 @@ class Usercommands(commands.Cog):
         embed.set_image(url=emoji.url)
         await ctx.send(embed=embed)
 
-
+    #returns the spotify status
     @commands.command()
     async def spotify(self, ctx, member:discord.Member = None):
         if member is None:
@@ -367,37 +234,8 @@ class Usercommands(commands.Cog):
 
 
 
+
     #error handling for the above
-    @listrole.error
-    async def listrole_error(self, ctx, error):
-        if isinstance(error, commands.RoleNotFound):
-            await ctx.send("You need to name a valid role!")
-        elif isinstance(error, commands.MissingPermissions):
-            await ctx.send("Nice try, but you don't have the permissions to do that!")
-        elif isinstance(error, commands.CommandInvokeError):
-            await ctx.send("I didn't find a good match for the role you provided. Please be more specific, or mention the role, or use the Role ID.")
-        else:
-            raise error
-
-    @roleinfo.error
-    async def roleinfo_error(self, ctx, error):
-        if isinstance(error, commands.RoleNotFound):
-            await ctx.send("You need to name a valid role!")
-        elif isinstance(error, commands.MissingPermissions):
-            await ctx.send("Nice try, but you don't have the permissions to do that!")
-        elif isinstance(error, commands.CommandInvokeError):
-            await ctx.send("I didn't find a good match for the role you provided. Please be more specific, or mention the role, or use the Role ID.")
-        else:
-            raise error
-        
-
-    @userinfo.error
-    async def userinfo_error(self, ctx, error):
-        if isinstance(error, commands.MemberNotFound):
-            await ctx.send("You need to mention a member, or just leave it blank.")
-        else:
-            raise error
-
     @avatar.error
     async def avatar_error(self, ctx, error):
         if isinstance(error, commands.MemberNotFound):
