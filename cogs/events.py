382facbdfb9e13725e762abd849f5987c7894c53
phxenix-w <80397293+phxenix-w@users.noreply.github.com>
2021-09-14 23:58:12 +0200

    improved some code in events

--- cogs/events.py
+++ cogs/events.py
@@ -36,25 +36,21 @@ class Events(commands.Cog):
     async def on_member_join(self, member):
         channel = self.bot.get_channel(739299507937738849) #ssbutg general: 739299507937738849
         rules = self.bot.get_channel(739299507937738843) #rules-and-info channel on ssbutg
-        guild = self.bot.get_guild(739299507795132486) #ssbutg smash discord
-        role = discord.utils.get(guild.roles, id=739391329779581008) #muted role
-        cadet = discord.utils.get(guild.roles, id=739299507799326843) #cadet role
+        muted_role = discord.utils.get(member.guild.roles, id=739391329779581008) #muted role
+        cadet = discord.utils.get(member.guild.roles, id=739299507799326843) #cadet role
 
-        try: #basically if the user id is in the muted.json this executes
-            with open(r'./json/muted.json', 'r') as f:
-                muted_users = json.load(f)
+        with open(r'./json/muted.json', 'r') as f:
+            muted_users = json.load(f)  
 
-            muted = muted_users[f'{member.id}']['muted'] #if a user is not muted, this here throws a keyerror, and then the except code kicks in
+        #checking if the user is muted when he joins
+        if f'{member.id}' in muted_users:
+            await member.add_roles(muted_role)
+            await member.add_roles(cadet)
+            await channel.send(f"Welcome back, {member.mention}! You are still muted, so maybe check back later.")
+            return
 
-            if muted is True:
-                await member.add_roles(role)
-                await member.add_roles(cadet)
-                await channel.send(f"Welcome back, {member.mention}! You are still muted, so maybe check back later.")
-
-
-
-        except:
-            await channel.send(f"{member.mention} has joined the ranks! What's shaking?\nPlease take a look at the {rules.mention} channel for information about server events/functions!")
+        #if not this is the normal greeting
+        await channel.send(f"{member.mention} has joined the ranks! What's shaking?\nPlease take a look at the {rules.mention} channel for information about server events/functions!")
 
 
     #if you join a voice channel, you get this role here
@@ -100,20 +96,20 @@ class Events(commands.Cog):
                 for role in color_roles:
                     try:
                         removerole = get(after.guild.roles, id=role)
-                        await after.remove_roles(removerole)
+                        if removerole in after.roles:
+                            await after.remove_roles(removerole)
                     except:
                         pass
         
         #this here gives out the cadet role on a successful member screening, on join was terrible because of shitty android app
         try:
-            cadetrole = discord.utils.get(before.guild.roles, id=739299507799326843)
-            member = self.bot.get_guild(before.guild.id).get_member(before.id)
             if before.bot or after.bot:
                 return
             else:
                 if before.pending == True:
                     if after.pending == False:
-                        await member.add_roles(cadetrole)
+                        cadetrole = discord.utils.get(before.guild.roles, id=739299507799326843)
+                        await after.add_roles(cadetrole)
         except AttributeError:
             pass
 
