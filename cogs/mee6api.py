1bbd27e04a82fdacd5e020da245624c6d8ce8687
phxenix-w <80397293+phxenix-w@users.noreply.github.com>
2021-09-16 01:40:23 +0200

    added new error case for updatelevel

--- cogs/mee6api.py
+++ cogs/mee6api.py
@@ -95,6 +95,8 @@ class Mee6api(commands.Cog):
             await ctx.send("This command can only be used in the SSBU TG Discord Server.")
         elif isinstance(error, commands.MemberNotFound):
             await ctx.send("Please mention a valid member, or leave it blank.")
+        elif isinstance(error, commands.CommandInvokeError):
+            await ctx.send("Something went wrong! Either the API is down or the user was not in the leaderboard yet. Please try again later.")
         else:
             raise error
 
