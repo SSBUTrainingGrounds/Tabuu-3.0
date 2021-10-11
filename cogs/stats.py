d550728954bbefa58849fcb68507be21152b5bc4
phxenix-w <80397293+phxenix-w@users.noreply.github.com>
2021-10-12 01:10:18 +0200

    cpu percentage small change

--- cogs/stats.py
+++ cogs/stats.py
@@ -152,7 +152,7 @@ class Stats(commands.Cog):
         embed.add_field(name="Bot Version:", value=self.bot.version_number, inline=True)
         embed.add_field(name="Python Version:", value=platform.python_version(), inline=True)
         embed.add_field(name="discord.py Version:", value=discord.__version__, inline=True)
-        embed.add_field(name="CPU Usage:", value=f"{psutil.cpu_percent(interval=0.2)}%", inline=True)
+        embed.add_field(name="CPU Usage:", value=f"{psutil.cpu_percent(interval=None)}%", inline=True)
         embed.add_field(name="RAM Usage:", value=f"{round(psutil.virtual_memory()[3]/(1024*1024*1024), 2)}GB/{round(psutil.virtual_memory()[0]/(1024*1024*1024), 2)}GB ({psutil.virtual_memory()[2]}%)", inline=True)
         embed.add_field(name="Uptime:", value=str(datetime.timedelta(seconds=uptimeSeconds)).split(".")[0], inline=True)
         embed.set_footer(text="Creator: Phxenix#1104, hosted on: Raspberry Pi 3B+")
