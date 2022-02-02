797e7f87a4202e9a87db6b72a1037a0e847dd569
phxenix-w <williamnolden98@gmail.com>
2022-02-02 20:08:36 +0100

    reformatted main file using black

--- main.py
+++ main.py
@@ -1,60 +1,69 @@
-#Tabuu 3.0
-#by Phxenix for SSBU Training Grounds
-#Version: 5.5.5
-#Last Changes: 29 January 2022
-#Report any bugs to: Phxenix#1104
-#
+# Tabuu 3.0
+# by Phxenix for SSBU Training Grounds
+# Version: 6.0.1
+# Last Changes: 2 February 2022
+# Contact me on Discord: Phxenix#1104
 
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 import os
 import utils.logger
 
 
-#
-#this main file here loads all the cogs and starts the bot obviously, also holds the reloadcogs command
-#
+# intents so the bot can track its users
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="%", intents=intents, status=discord.Status.online)
+# for a custom help command
+bot.remove_command("help")
 
-intents = discord.Intents.all() #intents so the bot can track its users
-bot = commands.Bot(command_prefix='%', intents=intents, status=discord.Status.online)
-bot.remove_command('help') #for a custom help command
-
-bot.version_number = "5.5.5" #the "version", maintain every now and then
+bot.version_number = "6.0.1"
 
 utils.logger.create_logger()
 
 
-#prints to the console when its ready
+# prints to the console when its ready
 @bot.event
 async def on_ready():
-    print(f"Lookin' good, connected as: {str(bot.user)}, at: {discord.utils.utcnow().strftime('%d-%m-%Y %H:%M:%S')} UTC")
-
-    
-#loads all of our cogs
-for filename in os.listdir(r'./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+    print(
+        f"Lookin' good, connected as: {str(bot.user)}, at: {discord.utils.utcnow().strftime('%d-%m-%Y %H:%M:%S')} UTC"
+    )
 
 
-#command for manually reloading all cogs, so i dont have to restart the bot for every change. can only be used by me
-#embeds can have up to 25 fields, we are at 20 cogs currently. need a different solution when we go over that
+# loads all of our cogs
+for filename in os.listdir(r"./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+
+
 @bot.command()
 @commands.is_owner()
 async def reloadcogs(ctx):
+    """
+    Command for manually reloading all cogs, so i dont have to restart the bot for every change.
+    Can only be used by the owner of the bot.
+    """
+    # embeds can have up to 25 fields, we are at 21 cogs currently. need a different solution when we go over that
     embed = discord.Embed(title="Reloading cogs...", colour=discord.Colour.blue())
-    for filename in os.listdir(r'./cogs'):
-        if filename.endswith('.py'):
+    for filename in os.listdir(r"./cogs"):
+        if filename.endswith(".py"):
             try:
-                bot.unload_extension(f'cogs.{filename[:-3]}')
-                bot.load_extension(f'cogs.{filename[:-3]}')
-                embed.add_field(name=f"✅ Successfully reloaded {filename[:-3]} ✅", value="Ready to go.", inline=False)
+                bot.unload_extension(f"cogs.{filename[:-3]}")
+                bot.load_extension(f"cogs.{filename[:-3]}")
+                embed.add_field(
+                    name=f"✅ Successfully reloaded {filename[:-3]} ✅",
+                    value="Ready to go.",
+                    inline=False,
+                )
             except Exception as exc:
-                embed.add_field(name=f"❌ FAILED TO RELOAD {filename[:-3]} ❌", value=exc, inline=False)
+                embed.add_field(
+                    name=f"❌ FAILED TO RELOAD {filename[:-3]} ❌",
+                    value=exc,
+                    inline=False,
+                )
     await ctx.send(embed=embed)
 
 
-#error handling for the reloadcogs command
 @reloadcogs.error
 async def reloadcogs_error(ctx, error):
     if isinstance(error, commands.NotOwner):
@@ -63,9 +72,8 @@ async def reloadcogs_error(ctx, error):
         raise error
 
 
-
-#run token, in different file because of security
-with open(r'./files/token.txt') as f:
+# run token, in different file because of security
+with open(r"./files/token.txt") as f:
     TOKEN = f.readline()
 
 bot.run(TOKEN)
