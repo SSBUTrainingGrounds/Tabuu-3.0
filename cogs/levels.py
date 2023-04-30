f440b624480ca871f41f0b2c065379a2072ba5f9
atomflunder <80397293+atomflunder@users.noreply.github.com>
2023-04-30 21:50:26 +0200

    Improve %rank command

--- cogs/levels.py
+++ cogs/levels.py
@@ -91,14 +91,7 @@ class Levels(commands.Cog):
 
         return (old_level, new_level, new_xp)
 
-    async def update_level_role(
-        self, member: discord.Member, level: int, guild: discord.Guild
-    ) -> Optional[discord.Role]:
-        """Assigns you a new role depending on your level and removes all of the other ones.
-        Returns the new role.
-        """
-        rolegiven = None
-
+    def get_all_level_roles(self, guild: discord.Guild) -> list[discord.Role]:
         defaultrole = discord.utils.get(guild.roles, id=TGLevelRoleIDs.RECRUIT_ROLE)
         level10 = discord.utils.get(guild.roles, id=TGLevelRoleIDs.LEVEL_10_ROLE)
         level25 = discord.utils.get(guild.roles, id=TGLevelRoleIDs.LEVEL_25_ROLE)
@@ -106,41 +99,94 @@ class Levels(commands.Cog):
         level75 = discord.utils.get(guild.roles, id=TGLevelRoleIDs.LEVEL_75_ROLE)
         level100 = discord.utils.get(guild.roles, id=TGLevelRoleIDs.LEVEL_100_ROLE)
 
-        levelroles = [defaultrole, level10, level25, level50, level75, level100]
+        return [defaultrole, level10, level25, level50, level75, level100]
 
-        async def assign_level_role(assign_role: discord.Role) -> discord.Role:
-            """Removes every other level role and assigns the correct one."""
+    async def assign_level_role(
+        self,
+        member: discord.Member,
+        levelroles: list[discord.Role],
+        assign_role: discord.Role,
+    ) -> discord.Role:
+        """Removes every other level role and assigns the correct one."""
 
-            roles_to_remove = [role for role in levelroles if role is not assign_role]
-            await member.remove_roles(*roles_to_remove)
-            await member.add_roles(assign_role)
-            return assign_role
+        roles_to_remove = [role for role in levelroles if role is not assign_role]
+        await member.remove_roles(*roles_to_remove)
+        await member.add_roles(assign_role)
+        return assign_role
+
+    async def update_level_role(
+        self, user: discord.User, level: int, guild: discord.Guild
+    ) -> Optional[discord.Role]:
+        """Assigns you a new role depending on your level and removes all of the other ones.
+        Returns the new role.
+        """
+        rolegiven = None
+
+        levelroles = self.get_all_level_roles(guild)
+
+        [defaultrole, level10, level25, level50, level75, level100] = levelroles
+
+        try:
+            member = await guild.fetch_member(user.id)
+        except discord.NotFound:
+            return
 
         if level >= 100:
             if level100 not in member.roles:
-                rolegiven = await assign_level_role(level100)
+                rolegiven = await self.assign_level_role(member, levelroles, level100)
 
         elif level >= 75:
             if level75 not in member.roles:
-                rolegiven = await assign_level_role(level75)
+                rolegiven = await self.assign_level_role(member, levelroles, level75)
 
         elif level >= 50:
             if level50 not in member.roles:
-                rolegiven = await assign_level_role(level50)
+                rolegiven = await self.assign_level_role(member, levelroles, level50)
 
         elif level >= 25:
             if level25 not in member.roles:
-                rolegiven = await assign_level_role(level25)
+                rolegiven = await self.assign_level_role(member, levelroles, level25)
 
         elif level >= 10:
             if level10 not in member.roles:
-                rolegiven = await assign_level_role(level10)
+                rolegiven = await self.assign_level_role(member, levelroles, level10)
 
         else:
             if defaultrole not in member.roles:
-                rolegiven = await assign_level_role(defaultrole)
+                rolegiven = await self.assign_level_role(
+                    member, levelroles, defaultrole
+                )
 
         return rolegiven
+
+    def get_next_role(
+        self, current_xp: int, current_level: int, guild: discord.Guild
+    ) -> tuple[int, int, Optional[discord.Role]]:
+        """Gets you the next role, if there is any, plus the levels and XP needed to get there."""
+        levelroles = self.get_all_level_roles(guild)
+
+        [_, level10, level25, level50, level75, level100] = levelroles
+
+        if current_level >= 100:
+            return (0, 0, None)
+
+        if current_level >= 75:
+            return (
+                100 - current_level,
+                self.get_xp_for_level(100) - current_xp,
+                level100,
+            )
+
+        if current_level >= 50:
+            return (75 - current_level, self.get_xp_for_level(75) - current_xp, level75)
+
+        if current_level >= 25:
+            return (50 - current_level, self.get_xp_for_level(50) - current_xp, level50)
+
+        if current_level >= 10:
+            return (25 - current_level, self.get_xp_for_level(25) - current_xp, level25)
+
+        return (10 - current_level, self.get_xp_for_level(10) - current_xp, level10)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
@@ -268,6 +314,10 @@ class Levels(commands.Cog):
     async def rank(self, ctx: commands.Context, user: discord.User = None) -> None:
         """Shows your level and xp, or the level and xp of another user."""
 
+        if ctx.guild.id != GuildIDs.TRAINING_GROUNDS:
+            await ctx.send("This command can only be used in the Training Grounds.")
+            return
+
         await ctx.typing()
 
         if user is None:
@@ -302,6 +352,10 @@ class Levels(commands.Cog):
         xp_progress = xp - self.get_xp_for_level(level)
         xp_needed = self.get_xp_for_level(level + 1) - self.get_xp_for_level(level)
 
+        [next_role_level, next_role_xp, next_role] = self.get_next_role(
+            xp, level, ctx.guild
+        )
+
         embed.add_field(name="Level", value=level, inline=False)
         embed.add_field(name="XP", value=f"{xp:,}", inline=False)
         embed.add_field(
@@ -309,6 +363,18 @@ class Levels(commands.Cog):
             value=f"{xp_progress:,}/{xp_needed:,} ({round((xp_progress / xp_needed) * 100, 2)}%)",
             inline=False,
         )
+        if next_role:
+            embed.add_field(
+                name="Next role",
+                value=f"{next_role.mention}\nLevels needed: {next_role_level}\nXP needed: {next_role_xp:,}",
+                inline=False,
+            )
+        else:
+            embed.add_field(
+                name="Next role",
+                value="No more roles to unlock!",
+                inline=False,
+            )
         embed.add_field(
             name="Rank",
             value=f"#{rank}",
@@ -318,7 +384,10 @@ class Levels(commands.Cog):
         if prev_user is not None:
             prev_member = self.bot.get_user(prev_user[1])
             if prev_member is None:
-                prev_member = await self.bot.fetch_user(prev_user[1])
+                try:
+                    prev_member = await self.bot.fetch_user(prev_user[1])
+                except (discord.NotFound, discord.HTTPException):
+                    prev_member = "Unknown User"
 
             embed.add_field(
                 name="User above",
@@ -329,7 +398,10 @@ class Levels(commands.Cog):
         if next_user is not None:
             next_member = self.bot.get_user(next_user[1])
             if next_member is None:
-                next_member = await self.bot.fetch_user(next_user[1])
+                try:
+                    next_member = await self.bot.fetch_user(next_user[1])
+                except (discord.NotFound, discord.HTTPException):
+                    next_member = "Unknown User"
 
             embed.add_field(
                 name="User below",
@@ -350,6 +422,10 @@ class Levels(commands.Cog):
     @app_commands.guilds(*GuildIDs.ALL_GUILDS)
     async def levels(self, ctx: commands.Context) -> None:
         """Shows the leaderboard for levels."""
+
+        if ctx.guild.id != GuildIDs.TRAINING_GROUNDS:
+            await ctx.send("This command can only be used in the Training Grounds.")
+            return
 
         await ctx.typing()
 
